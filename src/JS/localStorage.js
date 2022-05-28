d53b926ada0c5412af783a9ed3d58e8d012784be
Jesse <akohgodwinjesse@gmail.com>
2022-05-28 21:31:31 +0100

    uploading to remote repo

--- src/JS/localStorage.js
+++ src/JS/localStorage.js
@@ -1,13 +1,13 @@
 const storeItem = (task) => {
-        let added;
-        if(localStorage.getItem('Azil_cart') === null){
-          added= [];
-        } else {
-          added = JSON.parse(localStorage.getItem('Azil_cart'));
-        }
-        added.push(task);
-      
-        localStorage.setItem('Azil_cart', JSON.stringify(added));
+    let added
+    if (localStorage.getItem('Azil_cart') === null) {
+        added = []
+        added.push(task)
+    } else {
+        added = JSON.parse(localStorage.getItem('Azil_cart'))
+        added.push(task)
+    }
+    localStorage.setItem('Azil_cart', JSON.stringify(added))
 }
 
 export default storeItem
