86675de3263e9a7ee6b7539da199fe033be3a74c
Jesse <akohgodwinjesse@gmail.com>
2022-05-28 21:30:25 +0100

    Update testimonial.vue

--- src/views/testimonial.vue
+++ src/views/testimonial.vue
@@ -1,40 +1,43 @@
 <template>
-  <main>
-      <h1>Testimonials</h1>
-      <div class="testimonial_list">
- <div data-aos="fade-up" data-aos-duration="2000" v-for="review in reviews" :key="review.id">
-      <UserStories  :review="review" />   
-    </div>
-      </div>
-   
-   <Products/>
-   <Footer />
- 
-  </main>
+    <main>
+        <h1>Testimonials</h1>
+        <div class="testimonial_list">
+            <div
+                data-aos="fade-up"
+                data-aos-duration="2000"
+                v-for="review in reviews"
+                :key="review.id"
+            >
+                <UserStories :review="review" />
+            </div>
+        </div>
+
+        <Products />
+        <Footer />
+    </main>
 </template>
 
 <script>
 import AOS from 'aos'
 import 'aos/dist/aos.css'
-import Footer from  '../components/footer.vue';
+import Footer from '../components/footer.vue'
 import Products from '../components/productsPack.vue'
-import UserStories from '../components/userStories.vue';
-import json from "../../data/reviews.json";
+import UserStories from '../components/userStories.vue'
+import json from '../../data/reviews.json'
 export default {
-name:'testimonials',
-components: {UserStories, Footer, Products},
-  setup(){
-      return{
-        reviews: json.posts
-      }
-  },
-created() {
-    AOS.init()
-}
-
+    name: 'testimonials',
+    components: { UserStories, Footer, Products },
+    setup() {
+        return {
+            reviews: json.posts,
+        }
+    },
+    created() {
+        AOS.init()
+    },
 }
 </script>
 
 <style lang="scss" scoped src="../scss/testimonial.scss">
- @import url('../scss/testimonial.scss');
+@import url('../scss/testimonial.scss');
 </style>
