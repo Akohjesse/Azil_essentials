65e4babe641c6d8ab745c805a6f823a008bfb006
Jesse <akohgodwinjesse@gmail.com>
2022-05-28 21:30:14 +0100

    Update quantity++.vue

--- src/components/quantity++.vue
+++ src/components/quantity++.vue
@@ -1,55 +1,53 @@
 <template>
-  <div class="items">
-      <div @click="reduceQuantity" class="reduce qtt">
-        <span>-</span>
-      </div>
-      <div class="value">{{quantity}}</div>
-      <div @click="increaseQuantity" class="increase qtt">
-         +
-      </div>
-  </div>
+    <div class="items">
+        <div @click="reduceQuantity" class="reduce qtt">
+            <span>-</span>
+        </div>
+        <div class="value">{{ quantity }}</div>
+        <div @click="increaseQuantity" class="increase qtt">+</div>
+    </div>
 </template>
 
 <script>
 export default {
- name: 'Quantity',
- data(){
-     return{ 
-         quantity: 1
-     }
- },
- methods:{
-     reduceQuantity(){
-         this.quantity--;
-         this.$emit('value', this.quantity)
-     },
-     increaseQuantity(){
-         this.quantity++;
-         this.$emit('value', this.quantity)
-     }
- }
+    name: 'Quantity',
+    data() {
+        return {
+            quantity: 1,
+        }
+    },
+    methods: {
+        reduceQuantity() {
+            this.quantity--
+            this.$emit('value', this.quantity)
+        },
+        increaseQuantity() {
+            this.quantity++
+            this.$emit('value', this.quantity)
+        },
+    },
 }
 </script>
 
 <style lang="scss" scoped>
-.items{
-    width:100px;
+.items {
+    width: 100px;
     padding: 8px;
     display: flex;
     justify-content: space-between;
     align-items: center;
-    border:1px solid grey;
-    .qtt{
+    border: 1px solid grey;
+    .qtt {
         font-size: 2em;
         font-weight: lighter;
         color: grey;
         cursor: pointer;
     }
-    .value{
-        font-family:Voyage;
+    .value {
+        font-family: Voyage;
         font-size: 1.3em;
         font-weight: bold;
-        color:rgb(136, 136, 136);
+        color: rgb(136, 136, 136);
     }
 }
 </style>
