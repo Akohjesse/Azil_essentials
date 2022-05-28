018f4524b8ed97136e5b6f60b461cea2e4a88afa
Jesse <akohgodwinjesse@gmail.com>
2022-05-28 21:30:48 +0100

    uploading to remote repo

--- src/JS/scroll.js
+++ src/JS/scroll.js
@@ -1,19 +1,18 @@
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger)
 
 var autoGrid = gsap.timeline({
-    scrollTrigger:{
-        trigger:'.message2',
-        start:"bottom center ",
+    scrollTrigger: {
+        trigger: '.message2',
+        start: 'bottom center ',
         ease: Power1.easeIn,
         toggleActions: 'play play resume reverse',
-    }
+    },
 })
 autoGrid.from('.knowledge .class', {
-    y:30,
-    opacity:0,
-    stagger:0.2,
+    y: 30,
+    opacity: 0,
+    stagger: 0.2,
 })
-
