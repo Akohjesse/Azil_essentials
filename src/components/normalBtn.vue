d641ff692c00c7e544062f59241b3090f34b6f9c
Jesse <akohgodwinjesse@gmail.com>
2022-05-28 21:31:18 +0100

    Update normalBtn.vue

--- src/components/normalBtn.vue
+++ src/components/normalBtn.vue
@@ -1,56 +1,55 @@
 <template>
-  <div class="continue display_flex">
-   <div class="enter">
-      <p>{{text}}</p>
-   </div>
-  <div class="arrow move_sideways">
-    <img src="../assets/arrow.svg" alt="">
-  </div> 
-</div>
+    <div class="continue display_flex">
+        <div class="enter">
+            <p>{{ text }}</p>
+        </div>
+        <div class="arrow move_sideways">
+            <img src="../assets/arrow.svg" alt="" />
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-name: 'Button',
-props: {
-    'text': String
-}
+    name: 'Button',
+    props: {
+        text: String,
+    },
 }
 </script>
 
 <style lang="scss" scoped>
-.continue{
+.continue {
     width: 100%;
-    margin-top:30px;
-    .arrow{
-      width:20%;
-      margin-left: 20px;
-     img{
-       width:100%;
-       transform: rotateY(180deg);
-     }
+    margin-top: 30px;
+    .arrow {
+        width: 20%;
+        margin-left: 20px;
+        img {
+            width: 100%;
+            transform: rotateY(180deg);
+        }
     }
-    .enter{
-    width: 60%;
-    height:auto;
-    border: 2px solid rgb(133, 133, 133);
-    align-items: center;
-    text-align: center;
-    padding:30px;
-    font-weight: 800;
-    color: black;
+    .enter {
+        width: 60%;
+        height: auto;
+        border: 2px solid rgb(133, 133, 133);
+        align-items: center;
+        text-align: center;
+        padding: 30px;
+        font-weight: 800;
+        color: black;
 
-    &:hover{
-      cursor:none;
-      color:orange;
-     transition: 0.4s ease-in-out;
-         box-shadow: rgb(87, 87, 87) 14px 11px 1px 0px;
+        &:hover {
+            cursor: none;
+            color: orange;
+            transition: 0.4s ease-in-out;
+            box-shadow: rgb(87, 87, 87) 14px 11px 1px 0px;
+        }
+        p {
+            font-family: voyage;
+            letter-spacing: 2px;
+        }
     }
-    p{
-      font-family: voyage;
-      letter-spacing: 2px;
-    }
-  }
-
-  }
+}
 </style>
