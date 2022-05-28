949dbc669363ce6bff6291ad988770fe0aba5089
Jesse <akohgodwinjesse@gmail.com>
2022-05-28 21:31:03 +0100

    uploading to remote repo

--- src/JS/cursor.js
+++ src/JS/cursor.js
@@ -1,11 +1,11 @@
-import curDot from 'cursor-dot';
+import curDot from 'cursor-dot'
 
 const cursor = curDot({
-        zIndex: 2,
-        diameter: 80,
-        borderWidth: 1,
-        borderColor: 'Orange',
-        easing: 10,
-        background: 'tranparent'
+    zIndex: 2,
+    diameter: 80,
+    borderWidth: 1,
+    borderColor: 'Orange',
+    easing: 10,
+    background: 'tranparent',
 })
 export default cursor
--- src/JS/revertnav.js
+++ src/JS/revertnav.js
@@ -1,4 +1,4 @@
-const revertNav = ()=>{
+const revertNav = () => {
     document.querySelector('.buttons').classList.toggle('open')
 }
 export default revertNav
