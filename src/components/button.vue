a27021ef67636eb3ae2314d5f9920b4372f821e0
Jesse <akohgodwinjesse@gmail.com>
2022-05-28 21:29:55 +0100

    Update button.vue

--- src/components/button.vue
+++ src/components/button.vue
@@ -1,22 +1,22 @@
 <template>
-  <button>{{text}}</button>
+    <button>{{ text }}</button>
 </template>
 
 <script>
 export default {
-name: 'Button',
-props:['text']
+    name: 'Button',
+    props: ['text'],
 }
 </script>
 
 <style lang="scss" scoped>
- button{
-     border: none;
-     padding:18px 20px;
-     outline: none;
-     background: rgb(158, 158, 158);
-     color: white;
-     font-family: Voyage;
-     font-size: 1em;
- }
+button {
+    border: none;
+    padding: 18px 20px;
+    outline: none;
+    background: rgb(158, 158, 158);
+    color: white;
+    font-family: Voyage;
+    font-size: 1em;
+}
 </style>
